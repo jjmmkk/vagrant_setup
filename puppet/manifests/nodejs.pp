03105de39ee010486c49590f5c3e211ee8c6183d
Jon-Morten Kristiansen <jon.morten.kristiansen@gmail.com>
2014-03-15 23:52:25 +0100

    Node.js setup: Fix Puppet errors
    1) Use Ubuntu instead of Debian, since the installation is easier there
    2) Install a newer, better and working Node.js package

--- puppet/manifests/nodejs.pp
+++ puppet/manifests/nodejs.pp
@@ -1,6 +1,21 @@
 import 'components/core.pp'
 
 
-class { 'nodejs':
-	manage_repo => true,
+class fix_nodejs_package {
+	class { '::apt': }
+	apt::ppa { 'ppa:chris-lea/node.js':}
 }
+
+class fix_nodejs_install inherits fix_nodejs_package {
+	class { '::nodejs':
+		version => 'latest',
+		require => Apt::Ppa['ppa:chris-lea/node.js'],
+	}
+	package { 'npm':
+		ensure => present,
+		provider => 'npm',
+		require => Class['::nodejs'],
+	}
+}
+
+class { 'fix_nodejs_install': }
